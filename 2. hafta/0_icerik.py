0ee239b984813cb1834fddabc550438a2f6ddc0a
Civa <huseyingunes@gmail.com>
2021-04-08 00:05:20 +0300

    2. ve 3. hafta anlatılacaklar

---
+++ 2. hafta/0_icerik.py
@@ -0,0 +1,7 @@
+'''
+1- Ekrana çıktı verme
+2- Matematiksel işlemler
+3- Farklı biçimlerde ekrana çıktı verme
+4- Özel karakterler
+5- Açıklama satırları
+'''
---
+++ 3. hafta/0_icerik.py
@@ -0,0 +1,35 @@
+'''
+1- Değişken nedir?
+2- Değişken tanımlama
+    Büyük küçük harf fark eder
+    Değişken isimleri nasıl olmalıdır?
+    Değişkenler genellikle nasıl isimlendirilir?
+3- Değişkeni ekrana yazdırma
+4- Değişken tipleri
+    "5" 5 arasındaki fark
+5- type
+6- Tip dönüştürme
+7- Çift tırnak tek tırnak
+8- Birden fazla değişkene aynı anda değer atama
+9- Kullanıcıdan giriş alma
+10- Kullanıcının girdiği değerin tipi ve tip dönüşümü
+11- Girilen iki sayıyı toplama
+12- Doğum yılından yaşı hesaplama
+13- Üçgenin iç açılarının ikisi girilince 3. yü hesaplama
+14- Vize final girilince ortalamayı hesaplama
+15- Bir çemberin çevresini ve alanını hesaplama
+17- e10 ile çok haneli sayı tanımlama
+18- String
+19- String uzunluğu
+20- String dilimleme
+21- strip
+22- replace
+23- iki stringi birleştirme
+24- format string ilişkisi
+25- string methods: https://www.w3schools.com/python/python_strings_methods.asp
+    Bunlar gösterilir ve kısaca anlatılır ama örnek yapılmaz
+26- count, find, title
+16- Boolean tipi
+    karşılaştırmaları boolean a atama
+    karşılaştırma ifadeleri kısaca bahset dersi bitir.
+'''
