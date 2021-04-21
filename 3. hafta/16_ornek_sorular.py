5d06419d4e262b232d2790503b01f9d5adb3e33f
Hüseyin GÜNEŞ <huseyingunes@gmail.com>
2021-04-21 09:32:39 +0300

    4. hafta düzeltme

--- 3. hafta/16_ornek_sorular.py
+++
@@ -1,4 +0,0 @@
-"""
-1- Girilen iki sayının ortalamasını hesaplayan algoritma
-2- Bir kenar uzunluğu ve o kenara ait yüksekliği girilen üçgenin alanını hesaplayan algoritma
-"""
---
+++ 3. hafta/16_ornekler.py
@@ -0,0 +1,12 @@
+"""
+1- Kullanıcının yaşını alarak doğduğu yılı hesaplayan program
+2- Finali ve ortalamasını alarak vize notunu hesaplayın
+3- Bir dikdörtgenin uzun ve kısa kenar değerlerini alarak alan ve çevresini hesaplayan program
+4- Problem konusundan örnekler ile geri kalanını tamamla
+5- Girilen iki sayının ortalamasını hesaplayan algoritma
+6- Bir kenar uzunluğu ve o kenara ait yüksekliği girilen üçgenin alanını hesaplayan algoritma
+7-
+8-
+9-
+10-
+"""
